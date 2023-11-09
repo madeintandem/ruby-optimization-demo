72592786c36e6c95295af34f7c5ee923d63cd403
Toby Leonard <toby.leonard@madeintandem.com>
2023-11-09 11:02:10 -0600

    Add rating param fetching

--- app/models/star_destroyer.rb
+++ app/models/star_destroyer.rb
@@ -1,2 +1,13 @@
 class StarDestroyer < ApplicationRecord
+  def jump_rating_params
+    @jump_rating_params ||= JumpRatingParam.where(star_destroyer_version: version).first
+  end
+
+  def combat_rating_params
+    @combat_rating_params ||= CombatRatingParam.where(star_destroyer_version: version).first
+  end
+
+  def interdiction_rating_param
+    @interdiction_rating_param ||= InterdictionRatingParam.where(star_destroyer_version: version).first
+  end
 end
