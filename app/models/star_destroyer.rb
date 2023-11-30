d95647ea856c469e7afb173f38954ed04949ec97
Toby Leonard <toby.leonard@madeintandem.com>
2023-11-30 15:28:32 -0600

    Add ship classes Fix typo

--- app/models/star_destroyer.rb
+++ app/models/star_destroyer.rb
@@ -1,4 +1,25 @@
+# frozen_string_literal: true
+
 class StarDestroyer < ApplicationRecord
+  CLASSES = %w[
+    Acclamator
+    Executor
+    Gladiator
+    Imperator
+    Imperial
+    Interdictor
+    Maelstrom
+    Mega
+    Onager
+    Qaz
+    Resurgent
+    Secutor
+    Tector
+    Venator
+    Victory
+    Xyston
+  ]
+
   def jump_rating_params
     @jump_rating_params ||= JumpRatingParam.where(star_destroyer_version: version).first
   end
@@ -7,7 +28,7 @@ class StarDestroyer < ApplicationRecord
     @combat_rating_params ||= CombatRatingParam.where(star_destroyer_version: version).first
   end
 
-  def interdiction_rating_param
-    @interdiction_rating_param ||= InterdictionRatingParam.where(star_destroyer_version: version).first
+  def interdiction_rating_params
+    @interdiction_rating_params ||= InterdictionRatingParam.where(star_destroyer_version: version).first
   end
 end
