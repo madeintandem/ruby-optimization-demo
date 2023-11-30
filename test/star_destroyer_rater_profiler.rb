695b9299f62b5eb10fed4123cfefe7f9df3952d7
Toby Leonard <toby.leonard@madeintandem.com>
2023-11-30 16:28:40 -0600

    Add more detailed benchmarking

--- test/star_destroyer_rater_profiler.rb
+++ test/star_destroyer_rater_profiler.rb
@@ -9,27 +9,30 @@ class StarDestroyerRaterProfiler
     ActiveRecord::Base.logger.level = Logger::INFO
 
     ActiveRecord::Base.transaction do
-      build_test_data(star_destroyer_count: 50)
+      benchmark = Benchmark.bm(15) do |bm|
+        bm.report('build test data') { build_test_data(star_destroyer_count: 50) }
 
-      # Build this here outside the profiling, so the only thing we do below
-      # is run the slow code. If the class' init is slow that may be something
-      # else to look into, but we're only concerned with the processing right
-      # now.
-      rater = StarDestroyerRater.new
+        # Build this here outside the profiling, so the only thing we do below
+        # is run the slow code. If the class' init is slow that may be something
+        # else to look into, but we're only concerned with the processing right
+        # now.
+        rater = nil
+        bm.report('build rater') { rater = StarDestroyerRater.new }
 
-      begin
-        profile = StackProf.run(mode: :cpu, interval: 1000, raw: true) do
-          seconds = Benchmark.realtime do
-            StarDestroyer.all.each do |star_destroyer|
-              rater.rate(star_destroyer)
+        begin
+          profile = StackProf.run(mode: :cpu, interval: 1000, raw: true) do
+            bm.report('rate all') do
+              StarDestroyer.all.each do |star_destroyer|
+                rater&.rate(star_destroyer)
+              end
             end
           end
         ensure
-          puts "The method took #{seconds.truncate(2)} seconds."
+          File.write('tmp/stackprof/star_destroyer_rater_perf.json', JSON.generate(profile))
         end
-      ensure
-        File.write('tmp/stackprof/star_destroyer_rater_perf.json', JSON.generate(profile))
       end
+      total_time = benchmark.map(&:real).reduce(0, &:+)
+      puts "Total time: #{total_time.truncate(2)} seconds"
 
       # Don't persist any model changes
       raise ActiveRecord::Rollback
